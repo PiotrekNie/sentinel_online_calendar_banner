1078edcac61d970993f4f658078df987726ab569
pniemirski <pniemirski@eversis.com>
2024-08-02 15:58:50 +0200

    CSOR-379 - update caledndat banner layout

--- src/store/useMonthStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-const useMonthStore = create((set) => ({
-  monthData: [],
-  setMonthData: (data) => set(() => ({ monthData: data })),
-}));
-
-export default useMonthStore;
